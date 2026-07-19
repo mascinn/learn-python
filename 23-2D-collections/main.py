b31d0662a3f3cacfff149f44b6f21be6ba5cd05b
mascin <makhasinmuhammad03@gmail.com>
2026-07-19 07:23:15 +0700

    learn 2D collections

---
+++ 23-2D-collections/main.py
@@ -0,0 +1,22 @@
+fruits = ["apple", "banana", "cooconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+meats = ["chicken", "fish", "turkey"]
+
+groceries = [fruits, vegetables, meats]
+print(groceries[0][2])
+
+for collection in groceries:
+    for food in collection:
+        print(food, end="")
+    print()
+
+
+numPad = ((1, 2, 3),
+          (4, 5, 6),
+          (7, 8, 9),
+          ("*", 0, "#"))
+
+for row in numPad:
+    for num in row:
+        print(num, end=" ")
+    print()
